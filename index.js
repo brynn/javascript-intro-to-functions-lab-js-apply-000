f8990a716f350d6ba3375e3563a84293cc80cc9b
brynn.shepherd@gmail.com <brynn.shepherd@gmail.com>
2018-08-20 23:30:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,6 +16,10 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   if (string === string.toLowerCase()) {
-    return 
+    return "I can\'t hear you!";
+  }
+  
+  if (string === string.toUpperCase()) {
+    
   }
 } 
